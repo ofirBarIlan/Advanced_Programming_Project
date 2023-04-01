9c0f71d7f02519a487949939e12bbe020301a104
tgote <tgote@10.0.0.10>
2023-04-01 11:57:46 -0700

    First Attempt

--- Book_Scrabble/src/test/ClientHandler.java
+++ Book_Scrabble/src/test/ClientHandler.java
@@ -2,8 +2,9 @@ package test;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-
+//Hi From tomer
 public interface ClientHandler {
 	void handleClient(InputStream inFromclient, OutputStream outToClient);
 	void close();
+	
 }

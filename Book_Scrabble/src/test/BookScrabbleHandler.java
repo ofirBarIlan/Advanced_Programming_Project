69dbb90b7c62f7486c1e1ad7aa2847bc3cd84fb8
Yanai Levi <yanailevi12@gmail.com>
2023-04-03 23:07:06 +0300

    add query and challenge methods to Dictionary manager

--- Book_Scrabble/src/test/BookScrabbleHandler.java
+++ Book_Scrabble/src/test/BookScrabbleHandler.java
@@ -1,4 +1,14 @@
 package test;
 
 public class BookScrabbleHandler {
+    //clientHandler
+    ClientHandler clientHandler;
+
+    public BookScrabbleHandler(ClientHandler clientHandler) {
+        this.clientHandler = clientHandler;
+    }
+
+    public void handleClient(InputStream inFromClient, OutputStream outToClient) {
+        clientHandler.handleClient(inFromClient, outToClient);
+    }
 }

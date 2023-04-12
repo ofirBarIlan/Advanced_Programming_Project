777a5432e259fc72c1dec93230294c8bda14fc93
Yanai Levi <yanailevi12@gmail.com>
2023-04-12 22:24:28 +0300

    fix MyServer class

--- Book_Scrabble/src/test/MyServer.java
+++ Book_Scrabble/src/test/MyServer.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -20,74 +17,87 @@ public class MyServer {
 	ServerSocket server;
 	ExecutorService executor;
 
-	//create a queue of sockets
-	Queue<Socket> queue = new ConcurrentLinkedQueue<Socket>();
 	
+	// constructor of MyServer
 	public MyServer(int port, ClientHandler ch, int maxThreads) {
 		this.port=port;
 		this.ch=ch;
 		this.maxThreads=maxThreads;
 	}
 	
+	// Creating the server's socket and start the server's operation in the background
 	public void start() {
 		stop=false;
 		try {
 			server = new ServerSocket(port);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		executor = Executors.newFixedThreadPool(this.maxThreads + 1);
-		executor.execute(()->startsServer());
-		
+		}		
+		executor = Executors.newFixedThreadPool(this.maxThreads); // the thread pool that will handle the clients (the maximum number of clients that will be handled parallelly is maxThreads)
+		new Thread(()->startsServer()).start(); //start the server's operation in the background (in new thread)
 	}
 	
 	private void startsServer() {
 		try {
-			//ServerSocket server = new ServerSocket(port);
-			server.setSoTimeout(1000);
+			server.setSoTimeout(1000); // in every second the server will wait again for the customer (it will repeat to the while condition)
 			while(!stop) {
 				try {
-					Socket client=server.accept();
-					queue.add(client);
-					// execute the client handler in a new thread, using the two arguments functional interface
-					executor.execute(()->handleClientInNewThread());
-					ch.close();
-					client.close();
-				}catch(SocketTimeoutException e) {}
+					Socket client=server.accept(); // wait for a client
+
+					// handle the client parallelly as a new task of the thread pool
+					executor.execute(()->{
+						try {
+							// create new instance of the client handler
+                            Class<? extends ClientHandler> clientHandlerClass = this.ch.getClass();
+                            ClientHandler currCh = clientHandlerClass.getDeclaredConstructor().newInstance();
+							
+                            currCh.handleClient(client.getInputStream(), client.getOutputStream()); // handle the client
+                            currCh.close(); // close the current client handler
+							client.close(); // close the client
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						finally {
+							if(!client.isClosed()) // if the client is not closed, so close it.
+								try {
+									client.close();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+						}
+					});
+					
+				} catch(SocketTimeoutException e) {}
+			}			
+			
+			executor.shutdown();
+			
+			// wait for tasks to complete execution 
+			try {
+				executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-//			server.close();
-	}catch(IOException e) {
-		e.printStackTrace();
+
+			server.close(); // close the server
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(!executor.isTerminated()) // if the executor is not terminated after the shutdown command, so do shutdownNow
+				executor.shutdownNow();
+			if(!server.isClosed()) // if the server is not closed, so close it.
+				try {
+					server.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 		}
 	}
 	
-	private Object handleClientInNewThread() {
-		Socket client = (Socket) queue.poll();
-		try {
-			ch.handleClient(client.getInputStream(), client.getOutputStream());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
+	//close the server's operation in the background and afterwards close the server
 	public void close() {
-		stop=true;
-		executor.shutdown();
-		
-		// wait until all the tasks have completed
-		try {
-			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		// close the server
-		try {
-			server.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		stop=true;		
 	}
 
 }

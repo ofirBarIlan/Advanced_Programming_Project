1916821fec6dc5c643177ada761a5101c02e456f
ofirBarIlan <ofir.gonen@live.biu.ac.il>
2023-06-05 23:44:52 +0300

    initial ORM

---
+++ demo/src/main/java/model/Game.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import test.*;
+
+public class Game implements Serializable{
+
+    // public enum GameState{
+    //     Idle,
+    //     WAITING_FOR_PLAYERS,
+    //     WAITING_FOR_START,
+    //     PLAYING,
+    //     FINISHED
+    // }
+    
+    private long gameId;
+    private Integer roomNumber;
+
+    // // Create the dictionary to store player hands
+    // Map<String, ArrayList<Tile>> playerHands = new HashMap<>();
+
+    // int curPlayerIndex;
+
+    // ArrayList<String> players;
+
+    // // Create the dictionary to store players score
+    // Map<String, Integer> playerScores = new HashMap<>();
+
+    // GameState gameState;
+
+    // Board board;
+
+    
+}

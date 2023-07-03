e857f6929fb339c145c9a8142ef435909292d1d7
ofirBarIlan <ofir.gonen@live.biu.ac.il>
2023-07-03 22:05:34 +0300

    Add mongoDB Project - scrabble

---
+++ Scrabble/src/main/java/MyPackage/GameData.java
@@ -0,0 +1,86 @@
+package MyPackage;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoException;
+import org.bson.Document;
+
+public class GameData implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public static int objectCounter=0;
+	ArrayList<String> names;
+	ArrayList<Integer> scores;
+	Integer numPlayers;
+	ArrayList<String> hands;
+	ArrayList<String> board;
+	String currentPlayer;
+	
+	// Default constructor
+	public GameData() {
+		
+		
+	}
+	// Constructor
+	public GameData(ArrayList<String> names, ArrayList<Integer> scores, ArrayList<String> hands, ArrayList<String> board, Integer numPlayers, String currentPlayer){
+		this.names=names;
+		this.scores=scores;
+		this.numPlayers=numPlayers;
+		this.hands=hands;
+		this.board=board;
+		this.currentPlayer=currentPlayer;		
+	}
+	
+	@Override
+	public String toString() {
+		return "GameData [names=" + names + ",currentPlayer="+currentPlayer.toString()+ "]";
+		
+	}
+
+	public Document toDocument( ) {
+		//Take all data members and append to Document;
+		Document document = new Document("serialVersionUID", serialVersionUID);
+		document.append("names",names);
+		document.append("scores",scores);
+		document.append("numPlayers",numPlayers);
+		document.append("hands",hands);
+		document.append("board", board);
+		document.append("currentPlayer", currentPlayer);
+		
+		return document;
+	}
+	
+	public static GameData fromDocument(Document d) {
+		
+		ArrayList<String> names = (ArrayList<String>) d.getList("names",String.class);
+		ArrayList<Integer> scores = (ArrayList<Integer>) d.getList("scores",Integer.class);
+		ArrayList<String> hands = (ArrayList<String>) d.getList("hands",String.class);
+		ArrayList<String> board = (ArrayList<String>) d.getList("board",String.class);
+		
+		String currentPlayer = d.getString("currentPlayer");
+		Integer numPlayers = d.getInteger("numPlayers");
+		
+		GameData gd = new GameData(names, scores, hands, board, numPlayers, currentPlayer);
+		return gd;		
+	}
+	
+	//public static intListFromDoc()
+	
+//	public MyPlayer(String name) {
+//		this.numbers=new ArrayList<Integer>();
+//		Random rand =new Random();
+//		numbers.add(9);
+//		numbers.add(11);
+//		this.numbers.add((Integer)rand.nextInt());
+//		this.numbers.add((Integer)rand.nextInt());
+//		this.numbers.add((Integer)rand.nextInt());
+//		this.numbers.add((Integer)rand.nextInt());
+//		this.name=name;
+//		objectCounter++;
+//		this.id=objectCounter;
+//	}
+}
---
+++ Scrabble/src/main/java/MyPackage/mongoTest.java
@@ -0,0 +1,103 @@
+package MyPackage;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoException;
+import org.bson.Document;
+import com.mongodb.client.model.Filters;
+public class mongoTest {
+	
+	// Connection settings
+		public static String connectionString = "mongodb://localhost:27017";
+		public static String databaseName = "MyDatabase";
+		// Create a MongoDB client
+		public static MongoClient  mongoClient = MongoClients.create(connectionString);
+		// Get a reference to the database
+		public static MongoDatabase  database = mongoClient.getDatabase(databaseName);
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//communicate with mongodb server:
+		// Convert GameState object to Document
+		System.out.println("test = "+testConnection(mongoClient));
+		
+		ArrayList<String> names = new ArrayList();
+		names.add("Yanai");
+		names.add("Ofir");
+		names.add("Tomer");
+		
+		ArrayList<Integer> scores = new ArrayList();
+		scores.add(3);
+		scores.add(6);
+		scores.add(2);
+		
+		Integer numPlayers = 3;
+		
+		ArrayList<String> hands = new ArrayList();
+		hands.add("-----cccccbbbbb");
+		hands.add("-----aaaaabbbbb");
+		hands.add("-----zzzzzbbbbb");
+		
+		ArrayList<String> board = new ArrayList();
+		board.add("-----cccccbbbbb");
+		board.add("-----aaaaabbbbb");
+		board.add("-----zzzzzbbbbb");
+		
+		String currentPlayer = "Tomer";
+		
+		GameData gd=new GameData(names, scores, hands, board, numPlayers, currentPlayer);
+		//MyPlayer p2=new MyPlayer("dani");
+		//MyPlayer p3=new MyPlayer("moshon");
+		Document gameDocument = gd.toDocument();
+		// Get a reference to the collection
+		MongoCollection<Document> game_collection = database.getCollection("GameSaves");
+		System.out.println(gd.names);
+		// Insert the document into the collection
+		game_collection.insertOne(gameDocument);
+		//player_collection.insertOne(p2.toDocument());
+		//player_collection.insertOne(p3.toDocument());
+		//now we want to get an object from the data base:
+		// Retrieve the document from the collection
+		
+		//Document playerdoc = player_collection.find(Filters.eq("name", "dani")).first();
+		System.out.println(getAllDocumentsMatching(game_collection,"currentPlayer","Tomer"));
+	
+		// Convert the Document to GameState object
+		//MyPlayer loadedPlayer = MyPlayer.fromDocument(playerdoc);
+		//System.out.println("loaded player is :"+loadedPlayer );
+	}
+	
+	
+	static ArrayList<Document> getAllDocumentsMatching(MongoCollection<Document> player_collection,String fieldName,String fieldValue){
+		ArrayList<Document> list=new ArrayList<Document>();
+		MongoCursor<Document> it=player_collection.find(Filters.eq(fieldName, fieldValue)).iterator();
+		while(it!=null&&it.hasNext()) {
+			list.add(it.next());
+		}
+		return list;
+	}
+	
+	public void saveGame(ArrayList<String> names, ArrayList<Integer> scores, ArrayList<String> hands, ArrayList<String> board, Integer numPlayers, String currentPlayer) {
+		
+	}
+	
+	public static boolean testConnection(MongoClient mongoClient) {
+        try {
+            // Attempt to retrieve the database names
+            String s=mongoClient.listDatabaseNames().first();
+            System.out.println("first data base is "+s);
+            return true;
+        } catch (Exception e) {
+        	e.printStackTrace();
+            return false;
+        }
+    }
+	
+	
+
+}
